7c4090b035111a22bdac51d4427450bb2e72cca9
Emmanuel Peña <1em.ppe.35@gmail.com>
2020-08-09 14:24:03 -0500

    getting reference to the layout bindin, app , datasouce, viewmodel factory and view model

--- app/src/main/java/com/emma/mycollaborators20/view/ui/fragments/CollaboratorListFragment.kt
+++ app/src/main/java/com/emma/mycollaborators20/view/ui/fragments/CollaboratorListFragment.kt
@@ -6,9 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 
 import com.emma.mycollaborators20.R
 import com.emma.mycollaborators20.databinding.CollaboratorListFragmentBinding
+import com.emma.mycollaborators20.model.localdb.CollaboratorDatabase
+import com.emma.mycollaborators20.viewmodel.CollaboratorListViewModel
+import com.emma.mycollaborators20.viewmodel.CollaboratorListViewModelFactory
 
 
 class CollaboratorListFragment : Fragment() {
@@ -19,8 +24,15 @@ class CollaboratorListFragment : Fragment() {
         //put all the dependencies wrap ALL layouts in a layout tag
         val binding: CollaboratorListFragmentBinding  = DataBindingUtil.inflate(inflater,
           R.layout.collaborator_list_fragment, container, false)
-        //
-        
+        //get reference to the app
+        val application = requireNotNull(this.activity).application
+        //reference to the data source
+        val dataSource = CollaboratorDatabase.getInstance(application).collaboratorDatabaseDao
+        //view model factory reference
+        val viewModelFactory = CollaboratorListViewModelFactory(dataSource, application)
+        //create a view model // CHECK IF THIS IS WORKING!!!!!!
+        val collaboratorListViewModel = ViewModelProvider(this,
+            viewModelFactory).get(CollaboratorListViewModel::class.java)
         return binding.root
     }
 
