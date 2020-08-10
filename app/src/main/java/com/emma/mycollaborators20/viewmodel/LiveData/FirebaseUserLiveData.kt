c596f1ea0a40062afbc2aaa1d377fc1e9ff7a626
Emmanuel Peña <1em.ppe.35@gmail.com>
2020-08-10 10:27:40 -0500

    setting up login

---
+++ app/src/main/java/com/emma/mycollaborators20/viewmodel/LiveData/FirebaseUserLiveData.kt
@@ -0,0 +1,2 @@
+package com.emma.mycollaborators20.viewmodel.LiveData
+
---
+++ app/src/main/java/com/emma/mycollaborators20/viewmodel/LoginViewModel.kt
@@ -0,0 +1,7 @@
+package com.emma.mycollaborators20.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
