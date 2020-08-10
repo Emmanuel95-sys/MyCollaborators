f9b6d87030726bafb697b4be8c11e16eff0bf119
Emmanuel Peña <1em.ppe.35@gmail.com>
2020-08-10 11:34:26 -0500

    updating dependencies, implementings FirebaseLiveData and login view model

--- app/src/main/java/com/emma/mycollaborators20/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/emma/mycollaborators20/viewmodel/LoginViewModel.kt
@@ -1,7 +1,19 @@
 package com.emma.mycollaborators20.viewmodel
 
+import androidx.lifecycle.map
 import androidx.lifecycle.ViewModel
+import com.emma.mycollaborators20.viewmodel.LiveData.FirebaseUserLiveData
 
 class LoginViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    enum class AuthenticationState {
+        AUTHENTICATED, UNAUTHENTICATED, INVALID_AUTHENTICATION
+    }
+
+    val authenticationState = FirebaseUserLiveData().map{user ->
+        if(user != null ){
+            AuthenticationState.AUTHENTICATED
+        }else{
+            AuthenticationState.UNAUTHENTICATED
+        }
+    }
 }
